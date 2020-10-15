d8e3d19a635492c090228cf813175db09fa55146
Henry Walshaw <henry.walshaw@gmail.com>
2020-10-15 11:43:32 +1100

    The version number has to be an integer
    Bit unfortunate really

--- {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/app.py
+++ {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/app.py
@@ -1,11 +1,9 @@
 import faust
 
-from . import __version__
-
 
 app = faust.App(
     '{{cookiecutter.app_name}}',
-    version=__version__,
+    version=1,
     autodiscover=True,
     origin='{{cookiecutter.app_name}}'   # imported name for this project (import proj -> "proj")
 )
