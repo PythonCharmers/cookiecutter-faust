6b45013c02b6d57593705c77cbed01f9bf6a1bb4
Henry Walshaw <henry.walshaw@gmail.com>
2020-10-15 13:28:06 +1100

    Better defaults for the Kafka broker and store

--- {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/app.py
+++ {{cookiecutter.app_name}}/{{cookiecutter.app_name}}/app.py
@@ -3,6 +3,8 @@ import faust
 
 app = faust.App(
     '{{cookiecutter.app_name}}',
+    broker='{{ cookiecutter.kafka_broker }}',
+    store='{{ cookiecutter.kafka_store }}',
     version=1,
     autodiscover=True,
     origin='{{cookiecutter.app_name}}'   # imported name for this project (import proj -> "proj")
